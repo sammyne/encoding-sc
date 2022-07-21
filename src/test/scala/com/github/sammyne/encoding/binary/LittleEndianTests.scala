c795479bbe16efffdb3df079e88643d194617eb1
xiangminli <xiangminli@alumni.sjtu.edu.cn>
2022-07-21 01:46:31 +0000

    feature(binary): impl LittleEndian

--- src/test/scala/com/github/sammyne/encoding/binary/LittleEndianTests.scala
+++ src/test/scala/com/github/sammyne/encoding/binary/LittleEndianTests.scala
@@ -2,20 +2,20 @@ package com.github.sammyne.encoding.binary
 
 import org.scalatest.funsuite.AnyFunSuite
 
-class BigEndianTests extends AnyFunSuite {
+class LittleEndianTests extends AnyFunSuite {
   test("int16") {
     case class TestCase(buf: Array[Byte], offset: Int, expect: Short)
 
     val testVector = Array(
-      TestCase(Array[Byte](0x12, 0x34, 0x56), 0, 0x1234),
-      TestCase(Array[Byte](0x12, 0x34, 0x56), 1, 0x3456)
+      TestCase(Array[Byte](0x12, 0x34, 0x56), 0, 0x3412),
+      TestCase(Array[Byte](0x12, 0x34, 0x56), 1, 0x5634)
     )
 
     for ((c, i) <- testVector.zipWithIndex) {
       val got = if (c.offset != 0) {
-        BigEndian.int16(c.buf, c.offset)
+        LittleEndian.int16(c.buf, c.offset)
       } else {
-        BigEndian.int16(c.buf)
+        LittleEndian.int16(c.buf)
       }
 
       assert(c.expect == got, s"#$i")
