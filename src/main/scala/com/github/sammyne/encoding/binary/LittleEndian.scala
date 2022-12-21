19f4a9684c5d3b229f9de5d4bb5a980ae7927a9c
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-12-21 03:58:00 +0000

    style: set up some plugins

--- src/main/scala/com/github/sammyne/encoding/binary/LittleEndian.scala
+++ src/main/scala/com/github/sammyne/encoding/binary/LittleEndian.scala
@@ -11,11 +11,11 @@ object LittleEndian extends ByteOrder {
 
   override def int64(buf: Array[Byte], offset: Int): Long = this.wrapBuffer(buf, offset).getLong()
 
-  override def putInt16(buf: Array[Byte], v: Short, offset: Int): Unit = this.wrapBuffer(buf,offset).putShort(v)
+  override def putInt16(buf: Array[Byte], v: Short, offset: Int): Unit = this.wrapBuffer(buf, offset).putShort(v)
 
-  override def putInt32(buf: Array[Byte], v: Int, offset: Int): Unit = this.wrapBuffer(buf,offset).putInt(v)
+  override def putInt32(buf: Array[Byte], v: Int, offset: Int): Unit = this.wrapBuffer(buf, offset).putInt(v)
 
-  override def putInt64(buf: Array[Byte], v: Long, offset: Int): Unit = this.wrapBuffer(buf,offset).putLong(v)
+  override def putInt64(buf: Array[Byte], v: Long, offset: Int): Unit = this.wrapBuffer(buf, offset).putLong(v)
 
   override def toString(): String = "LittleEndian"
 
